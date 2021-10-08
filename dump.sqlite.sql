e8bd9a7302040d4030cbb85b4d456cca4617ad79
Ailen Amodio <46222455+ailenamodio@users.noreply.github.com>
2021-10-08 11:14:29 -0300

    Add files via upload
    Ejercitación en sql 1

---
+++ dump.sqlite.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE curso(
+  codigo_curso integer PRIMARY KEY NOT NULL,
+  nombre varchar not NULL,
+  descripcion varchar,
+  turno varchar not NULL,
+  cupo integer not NULL
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
