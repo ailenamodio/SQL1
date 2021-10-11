ce96d65ef61eb19496545706fd14db1d99bec816
Ailen Amodio <46222455+ailenamodio@users.noreply.github.com>
2021-10-11 11:15:53 -0300

    Add files via upload
    Tema 3 - Queries

---
+++ 3.sql
@@ -0,0 +1,8 @@
+SELECT nombre, apellido, fecha_nacimiento FROM profesor ORDER BY fecha_nacimiento;
+SELECT * FROM profesor WHERE salario>=65000;
+SELECT * FROM profesor WHERE fecha_nacimiento BETWEEN '1980-01-01' AND '1989-12-31';
+SELECT * FROM profesor LIMIT 5;
+SELECT * FROM profesor WHERE apellido LIKE 'P%';
+SELECT * FROM profesor WHERE fecha_nacimiento BETWEEN '1980-01-01' AND
+'1989-12-31' AND salario > 80000;
+
